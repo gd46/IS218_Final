afe3ace5f1ae5f2977f8ac8faa93e29753a9b3b2
Giuseppe DiBella <giuseppedb@Giuseppes-MacBook-Pro-2.local>
2014-12-03 15:01:10 -0500

    Added autoloader, connected to database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
+require 'Autoloader.php';
+spl_autoload_register('Autoloader::loader');
+//$sql = 'SELECT UNITID, MAX(EFYTOTLT) FROM enrollment';
+
+require('connect.php');
+$sql = 'SELECT * FROM enrollment';
+
+$sth = $dbh->prepare($sql);
+$sth->execute();
+$results = $sth->fetchAll();
+print_r($results);
 ?>
